3e0a6935b01adc9d5d5de07aacb9ae9e7c2289a1
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-02-06 11:18:16 +0530

    writing onValue function and retrivel code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://addtocart-da06f-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -11,9 +11,43 @@ const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value;
+    let inputValue = inputFieldEl.value
+    
     push(shoppingListInDB, inputValue)
-    console.log(`${inputValue} added to database`)
+    
+    clearInputFieldEl()
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.entries(snapshot.val())
+    
+    clearShoppingListEl()
+    
+    for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+        appendItemToShoppingListEl(currentItem)
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+    // shoppingListEl.innerHTML += `<li>${itemValue}</li>` //with this we cannot set onClickListener so change
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+    shoppingListEl.append(newEl)
+}
